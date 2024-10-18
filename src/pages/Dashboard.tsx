209737bdb1790f06fbfaa033437dd08494c476ab
asif <asif69284@gmail.com>
2024-10-18 16:36:28 +0545

    Setup box styles

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, Container, Heading, Text } from '@chakra-ui/react'
 
 export default function Dashboard() {
-    return (
-        <div>
 
-        </div>
+    const boxStyles = {
+        p: "10px",
+        bg: "purple.400",
+        color: "white",
+        m: "10px",
+        textAlign: "center",
+        filter: "blur(2px)",
+        ':hover': {
+            color: 'black',
+            bg: 'blue.300'
+        }
+    }
+
+    return (
+        <Container as={"section"} maxWidth={"4xl"}>
+            <Heading my={"30px"} p={"10px"}>Chakra UI Components</Heading>
+            <Text ml={"30px"} mb={"10px"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem repellendus sunt mollitia corporis eligendi.</Text>
+            <Text ml={"30px"} color={"blue.400"} fontWeight={"bold"}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat quidem repellendus sunt mollitia corporis eligendi.</Text>
+            <Box my={"30px"} p={"20px"} bg={"orange"}>
+                <Text color={"white"}>This is a box component</Text>
+            </Box>
+            <Box sx={boxStyles}>
+                Hello Nijja
+            </Box>
+        </Container>
     )
 }
