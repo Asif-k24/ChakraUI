5a073528b590ca64f800ccb55664e84bf4b3192f
asif <asif69284@gmail.com>
2024-10-18 16:16:28 +0545

    minute change

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react'
-import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
