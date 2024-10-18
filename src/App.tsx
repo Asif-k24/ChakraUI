95fbf796805e85869a2b9eace8ed525b2e7d17af
asif <asif69284@gmail.com>
2024-10-18 10:58:53 +0545

    Using chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom'
+
+// layouts and pages
+import RootLayout from './layouts/RootLayout'
+import Dashboard from './pages/Dashboard'
+import Create from './pages/Create'
+import Profile from './pages/Profile'
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />}>
+      <Route index element={<Dashboard />} />
+      <Route path='create' element={<Create />} />
+      <Route path='profile' element={<Profile />} />
+    </Route>
+  )
+)
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
