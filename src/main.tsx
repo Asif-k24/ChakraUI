fb7cc404ff24bf7f2e4c171f4b19273e2f9a39f3
asif <asif69284@gmail.com>
2024-10-24 10:28:00 +0545

    Added fontbody & headingvv

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,19 @@ import App from './App.tsx'
 const customTheme = extendTheme({
   colors: {
     brand: {
-      100: '#f7fafc',
-      900: '#1a202c'
+      900: '#024fc9',
+      800: '#146af5',
+      700: '#2977f2',
+      600: '#337df2',
+      500: '#4287f5'
     }
+  },
+
+  fonts: {
+    body: 'Courier New',
+    heading: 'Courier New'
   }
+
 })
 
 createRoot(document.getElementById('root')!).render(
